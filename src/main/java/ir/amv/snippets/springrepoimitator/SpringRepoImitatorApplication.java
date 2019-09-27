47cbdddbe541c999daace63318994ad831d8daa2
Amir <AmirMV2006@gmail.com>
2019-09-27 16:05:56 +0200

    Initial commit

---
+++ src/main/java/ir/amv/snippets/springrepoimitator/SpringRepoImitatorApplication.java
@@ -0,0 +1,13 @@
+package ir.amv.snippets.springrepoimitator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepoImitatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepoImitatorApplication.class, args);
+	}
+
+}
